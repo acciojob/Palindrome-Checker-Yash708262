28ac67cd02e7d39affc794c32608b93b3f5cb7a8
Yash708262 <yash708262@gmail.com>
2025-09-20 17:10:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // complete the given function
 
-function palindrome(str){
-	let reverse = str.split("").reverse().join("");
-	if(reverse === str){
-		return true;
-	} else {
-		return false
-	}
+function palindrome(str) {
+  // Lowercase karo aur non-alphanumeric characters remove karo
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversed = cleaned.split("").reverse().join("");
+  return reversed === cleaned;
 }
-module.exports = palindrome
+
