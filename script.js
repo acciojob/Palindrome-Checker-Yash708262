fe928880e571c71a6dc75cbd9d6c37a94f71c35f
Yash708262 <yash708262@gmail.com>
2025-09-20 17:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+	let reverse = str.split("").reverse().join("");
+	if(reverse === str){
+		return true;
+	} else {
+		return false
+	}
 }
 module.exports = palindrome
